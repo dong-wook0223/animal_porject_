7a5e01f6a3afc5d6b65d4ee6e7b532e124531af9
BYAlPhA <chocbycho@gmail.com>
2026-03-11 02:24:58 +0900

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+// 배포용 코드(Vercel 환경변수 적용 버전)
 // const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 // const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+// 로컬 개발용 코드(직접 키 입력 버전) !!!반드시 배포 시에는 위의 배포용 코드로 변경할 것!!!
 const supabaseUrl = "https://fblhpysfqmepadjmqufi.supabase.co"
 const supabaseAnonKey = "sb_publishable_qw0frVOFgl1YSdfO5pQRNQ_mVV4B1Gv"
 
