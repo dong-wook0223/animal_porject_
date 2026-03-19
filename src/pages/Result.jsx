6661a8ec242925d343bf66d7358f5e21d628e5da
BYAlPhA <chocbycho@gmail.com>
2026-03-19 19:33:26 +0900

    Update Result.jsx

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -14,6 +14,7 @@ import { rankDogs, buildExplain } from "../domain";
 import { useQuiz } from "../store/quizStore";
 
 import { saveResults, useStatsData } from "../lib/hooks";
+import { saveCritics } from "../lib/analytics";
 
 // Fix for default marker icon in Leaflet + Vite
 import icon from 'leaflet/dist/images/marker-icon.png';
